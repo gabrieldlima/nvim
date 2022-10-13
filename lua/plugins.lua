52c532282167f3cc642a6d25191f7fef244b8522
Gabriel Lima <gabrielgsdlima@gmail.com>
2022-10-12 22:47:46 -0300

    New colorscheme: github-nvim-theme

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,4 +1,7 @@
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+  
+    -- Colorscheme
+    use 'projekt0n/github-nvim-theme'
 end)
