ac68e3eb128bcacc1caca7134cd4a9644f5c4466
Gabriel Lima <gabrielgsdlima@gmail.com>
2022-10-20 20:20:27 -0300

    pcall() && new configuration

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,3 +1,13 @@
-require("github-theme").setup({
-    theme_style = "dark_default"
+local status_ok, github_nvim_theme = pcall(require, "github-theme")
+if not status_ok then
+    return
+end
+
+github_nvim_theme.setup({
+    theme_style = "dark_default",
+    dark_sidebar = true,
+    comment_style = "italic",
+    keyword_style = "italic",
+    function_style = "NONE",
+    variable_style = "NONE"
 })
