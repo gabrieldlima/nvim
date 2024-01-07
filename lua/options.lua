e14117c9829b00b0de16e286b6e5011bf551b2a2
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-01-07 18:59:32 -0300

    Setting options

---
+++ lua/options.lua
@@ -0,0 +1,48 @@
+local o = vim.o
+
+-- UTF-8
+o.fileencoding = "utf-8"
+
+-- Make line numbers default
+o.number = true
+
+-- Show the line number relative to the line with the cursor
+o.relativenumber = true
+
+-- Tabs
+o.expandtab = true
+o.tabstop = 2
+o.softtabstop = 2
+o.shiftwidth = 2
+
+-- When there is a previous search pattern, highlight all its matches
+o.hlsearch = false
+
+-- Case-insensitive searching
+o.ignorecase = true
+o.smartcase = true
+
+-- No wrap long lines
+o.wrap = false
+
+-- Show (partial) command in the last line of the screen
+o.showcmd = false
+
+-- Highlight the text line of the cursor
+o.cursorline = true
+
+-- Decrease update time
+o.updatetime = 250
+o.timeoutlen = 300
+
+-- Save undo history
+o.undofile = false
+
+-- Sync clipboard between OS and Neovim
+o.clipboard = "unnamedplus"
+
+-- Disable mouse support
+o.mouse = ""
+
+-- Enable 24-bit RGB color
+o.termguicolors = true
