ac2c4e0d48e65171ced138da6ac074b66824e713
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-01-11 20:10:50 -0300

    Lazy plugin: telescope (fuzzy finder)

--- lua/lazyvim.lua
+++ lua/lazyvim.lua
@@ -13,14 +13,33 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   -- [[ Colorscheme ]]
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+  },
 
   -- [[ LSP Plugins ]]
   {
     -- Quickstart configs for Nvim LSP
     "neovim/nvim-lspconfig",
-  }
+  },
+
+  -- [[ Fuzzy Finder (files, lsp, etc) ]]
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      -- Fuzzy Finder Algorithm which requires local dependencies to be build.
+      -- Only load if `make` is avaliable.
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        cond = function()
+          return vim.fn.executable "make" == 1
+        end,
+      },
+    },
+  },
 })
