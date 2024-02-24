8e5ce0f335a1e19559a1ba8ef1eb3bb98517d33e
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-02-24 15:12:02 -0300

    Lazy plugin: nvim-surround

--- lua/lazyvim.lua
+++ lua/lazyvim.lua
@@ -227,6 +227,13 @@ require("lazy").setup({
     opts = {},
   },
 
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    opts = {},
+  },
+
   --
   -- [[ Colorscheme ]]
   --
