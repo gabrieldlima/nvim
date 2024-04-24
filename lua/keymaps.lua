ee7ac6218121428fecf2891dcf1d5e3646fa0aab
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-04-23 21:10:09 -0300

    Vim users: we don't use arrow keys here

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,6 +1,12 @@
 -- Map Esc to `jk`
 vim.keymap.set({ "i" }, "jk", "<Esc>", { silent = true })
 
+-- TIP: Disable arrow keys in normal mode
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
 -- Clear highlight search in normal mode
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
