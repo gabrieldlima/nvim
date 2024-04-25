6940b00c84ea85a3a20f8c0422edc814cfd1be9d
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-04-24 21:23:29 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -3,29 +3,5 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- [[ Setting options ]]
-require("options")
-
--- [[ Basic keymaps ]]
-require("keymaps")
-
--- [[ Install lazy.nvim plugin manager ]]
-require("lazyvim")
-
--- [[ Configure plugins ]]
-require("colorscheme")
-
--- [[ Configure Telescope ]]
-require("nvim-telescope")
-
--- [[ Configure Treesitter ]]
-require("treesitter")
-
--- [[ Configure LSP ]]
-require("lsp")
-
--- [[ Configure nvim-cmp ]]
-require("nvim-cmp")
-
--- [[ Configure lualine ]]
-require("statusline")
+require("config")
+require("plugins")
