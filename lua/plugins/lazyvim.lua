8a53d9884a01d5880e70d96764b5c4d5d95507f4
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-04-25 13:02:26 -0300

    Update lazyvim bootstrap function

--- lua/plugins/lazyvim.lua
+++ lua/plugins/lazyvim.lua
@@ -2,7 +2,7 @@
 -- [[ LazyVim plugin manager ]]
 --
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
