1d106dd8f72e3bafc787b09b5d2cb943653b210e
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-06-01 16:48:09 -0300

    Starting a new rewrite...

--- init.lua
+++ init.lua
@@ -12,4 +12,21 @@
 vim.g.mapleader = ","
 vim.g.maplocalleader = ","
 
-require("plugins")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({ import = "custom/plugins" }, {
+  change_detection = {
+    notify = false,
+  }
+})
