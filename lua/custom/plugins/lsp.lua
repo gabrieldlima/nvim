d9ef0f59c7191f98706310cd7e0da01c1f5a55a2
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-06-02 16:05:05 -0300

    feat: add which-key

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -12,15 +12,6 @@ return {
 
       -- Useful status updates for LSP
       { "j-hui/fidget.nvim", opts = {} },
-
-      {
-        "folke/which-key.nvim",
-        event = "VeryLazy",
-        init = function()
-          vim.o.timeout = true
-          vim.o.timeoutlen = 300
-        end,
-      }
     },
     config = function ()
       local builtin = require("telescope.builtin")
@@ -70,24 +61,6 @@ return {
           vim.lsp.buf.format()
         end, { desc = 'Format current buffer with LSP' })
       end
-
-      -- document existing key chains
-      require("which-key").register {
-        ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-        ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-        ["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
-        ["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
-        ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-        ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-        ["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-        ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-      }
-      -- register which-key VISUAL mode
-      -- required for visual <leader>hs (hunk stage) to work
-      require('which-key').register({
-        ['<leader>'] = { name = 'VISUAL <leader>' },
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
 
       -- mason-lspconfig requires that these setup functions are called in this order
       -- before setting up the servers.
