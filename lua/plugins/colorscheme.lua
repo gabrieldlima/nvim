cdf370713975fb62b6667413862678140e5f731c
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-07-24 21:23:06 -0300

    Catppuccin theme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+
+    config = function ()
+      require("catppuccin").setup {
+        flavor = "mocha",
+        color_overrides = {
+          mocha = {
+            base   = "#010101",
+            mantle = "#010101",
+            crust  = "#010101",
+          },
+        },
+        custom_highlights = function (colors)
+          return {
+            WinSeparator = { bg = colors.base, fg = colors.blue },
+          }
+        end
+      }
+      vim.cmd.colorscheme("catppuccin")
+    end
+  }
+}
