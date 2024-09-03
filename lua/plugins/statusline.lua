acaa90a750ac4bfb303aa0b215b178e50a920644
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-09-02 22:46:41 -0300

    custom icons for lualine

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -8,6 +8,19 @@ return {
         options = {
           theme = "catppuccin",
           globalstatus = true,
+          component_separators = "|",
+          section_separators = {left = '', right = ''},
+        },
+        sections = {
+          -- +-------------------------------------------------+
+          -- | A | B | C                             X | Y | Z |
+          -- +-------------------------------------------------+
+          lualine_a = {
+            {"mode", icon = "", separator = { left = "", right = "" }, right_padding = 2 }
+          },
+          lualine_z = {
+            {"location", separator = { left = "", right = "" }, left_padding = 2 }
+          },
         },
       }
     end
