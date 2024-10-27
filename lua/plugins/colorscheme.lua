967d3f039700234d89a1e6501bad04b04148858c
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-10-27 09:59:15 -0300

    plugin settings in opts

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,32 +4,34 @@ return {
     name = "catppuccin",
     priority = 1000,
 
-    config = function ()
-      require("catppuccin").setup {
-        flavor = "mocha",
-        color_overrides = {
-          mocha = {
-            base   = "#010101",
-            mantle = "#010101",
-            crust  = "#010101",
-          },
+    opts = {
+      flavor = "mocha",
+      color_overrides = {
+        mocha = {
+          base   = "#010101",
+          mantle = "#010101",
+          crust  = "#010101",
         },
-        custom_highlights = function (colors)
-          return {
-            WinSeparator = { bg = colors.base, fg = colors.blue },
-          }
-        end,
-        integrations = {
-          cmp = true,
-          fidget = true,
-          indent_blankline = {
-            enabled = true,
-            scope_color = "lavender",
-          },
-          telescope = true,
-          treesitter = true,
+      },
+      custom_highlights = function (colors)
+        return {
+          WinSeparator = { bg = colors.base, fg = colors.blue },
+        }
+      end,
+      integrations = {
+        cmp = true,
+        fidget = true,
+        indent_blankline = {
+          enabled = true,
+          scope_color = "lavender",
         },
-      }
+        telescope = true,
+        treesitter = true,
+      },
+    },
+
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
       vim.cmd.colorscheme("catppuccin")
     end
   }
