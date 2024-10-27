0e2c098c748bdb48affb1176809308ecb1a790d5
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-10-27 08:33:23 -0300

    Diagnostic keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,7 @@
 vim.keymap.set({ "i" }, "jk", "<Esc>", { silent = true })
 
 -- Clear highlights on search when pressing <Esc> in normal mode
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Diagnostic keymaps
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
