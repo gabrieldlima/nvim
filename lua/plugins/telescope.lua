ae51f0601971a7140b8e7b82b668a9e47975470e
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-10-27 10:57:10 -0300

    plugin settings in opts

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,6 +5,7 @@ return {
     event = "VimEnter",
     dependencies = {
       "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
       {
         "nvim-telescope/telescope-fzf-native.nvim",
         build = "make",
@@ -15,28 +16,33 @@ return {
       { "nvim-telescope/telescope-ui-select.nvim" },
     },
 
-    config = function ()
-      require("telescope").setup {
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown(),
-          },
+    opts = {
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown(),
         },
-      }
+      },
+    },
+
+    config = function (_, opts)
+      require("telescope").setup(opts)
 
       pcall(require("telescope").load_extension, "fzf")
       pcall(require("telescope").load_extension, "ui-select")
 
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ss", builtin.builtin)
-      vim.keymap.set("n", "<leader>sf", builtin.find_files)
-      vim.keymap.set("n", "<leader>sh", builtin.help_tags)
-      vim.keymap.set("n", "<leader>st", builtin.git_files)
-      vim.keymap.set("n", "<leader>sg", builtin.live_grep)
-      vim.keymap.set("n", "<leader>sw", builtin.grep_string)
-      vim.keymap.set("n", "<leader>sk", builtin.keymaps)
-      vim.keymap.set("n", "<leader>sd", builtin.diagnostics)
-      vim.keymap.set("n", "<leader>sb", builtin.buffers)
+      vim.keymap.set("n", "<leader>sb", builtin.buffers,     { desc = "[S]earch [B]uffers"})
+      vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+      vim.keymap.set("n", "<leader>sf", builtin.find_files,  { desc = "[S]earch [F]iles" })
+      vim.keymap.set("n", "<leader>sg", builtin.live_grep,   { desc = "[S]earch by [G]rep" })
+      vim.keymap.set("n", "<leader>sh", builtin.help_tags,   { desc = "[S]earch [H]elp" })
+      vim.keymap.set("n", "<leader>sk", builtin.keymaps,     { desc = "[S]earch [K]eymaps" })
+      vim.keymap.set("n", "<leader>ss", builtin.builtin,     { desc = "[S]earch [S]elect Telescope" })
+      vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+
+      vim.keymap.set("n", "<leader>sn", function ()
+        builtin.find_files { cwd = vim.fn.stdpath "config", prompt_title = "Neovim Config" }
+      end, { desc = "[S]earch [N]eovim [C]onfig" })
     end
   }
 }
