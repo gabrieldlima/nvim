b553d5c85c5765253d575a7b99f1af7526f72a3b
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-10-27 10:00:35 -0300

    plugin settings in opts

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -3,26 +3,24 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
 
-    config = function ()
-      require("lualine").setup {
-        options = {
-          theme = "catppuccin",
-          globalstatus = true,
-          component_separators = "|",
-          section_separators = {left = '', right = ''},
+    opts =  {
+      options = {
+        theme = "catppuccin",
+        globalstatus = true,
+        component_separators = "|",
+        section_separators = {left = '', right = ''},
+      },
+      sections = {
+        -- +-------------------------------------------------+
+        -- | A | B | C                             X | Y | Z |
+        -- +-------------------------------------------------+
+        lualine_a = {
+          {"mode", icon = "", separator = { left = "", right = "" }, right_padding = 2 }
         },
-        sections = {
-          -- +-------------------------------------------------+
-          -- | A | B | C                             X | Y | Z |
-          -- +-------------------------------------------------+
-          lualine_a = {
-            {"mode", icon = "", separator = { left = "", right = "" }, right_padding = 2 }
-          },
-          lualine_z = {
-            {"location", separator = { left = "", right = "" }, left_padding = 2 }
-          },
+        lualine_z = {
+          {"location", separator = { left = "", right = "" }, left_padding = 2 }
         },
-      }
-    end
+      },
+    }
   }
 }
