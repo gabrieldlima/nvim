eebb332369cf9a7cc6c8b50ae5616c749cc9e6e4
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-10-27 10:19:01 -0300

    plugin settings in opts

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,41 +3,49 @@ return {
     "nvim-treesitter/nvim-treesitter",
     enabled = true,
     build = ":TSUpdate",
+    main = "nvim-treesitter.configs", -- Sets main module to use for opts
 
-    config = function ()
-      require("nvim-treesitter.configs").setup {
-        ensure_installed = {
-          "bash",
-          "c",
-          "json",
-          "lua",
-          "markdown",
-          "nix",
-          "python",
-          "rust",
-        },
+    opts = {
+      ensure_installed = {
+        "bash",
+        "c",
+        "diff",
+        "html",
+        "json",
+        "lua",
+        "luadoc",
+        "markdown",
+        "markdown_inline",
+        "nix",
+        "python",
+        "query",
+        "rust",
+        "vim",
+        "vimdoc",
+      },
 
-        sync_install = false,
-        auto_install = true,
-        ignore_install = {},
+      sync_install = false,
+      auto_install = true,
+      ignore_install = {},
 
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
+      highlight = {
+        enable = true,
+        disable = {},
+        additional_vim_regex_highlighting = false,
+      },
+      indent = {
+        enable = true,
+        disable = {},
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          scope_incremental = false,
+          node_incremental = "<C-space>",
+          node_decremental = "<bs>",
         },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<C-space>",
-            node_incremental = "<C-space>",
-            scope_incremental = false,
-            node_decremental = "<bs>",
-          },
-        },
-        indent = {
-          enable = true,
-        },
-      }
-    end
+      },
+    }
   }
 }
