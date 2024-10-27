62b2ce66e34c8455dce443d235270246f82b793c
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-10-27 08:31:04 -0300

    Clear highlights on search when pressing <Esc> in normal mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,2 +1,5 @@
 -- Map Esc to `jk`
 vim.keymap.set({ "i" }, "jk", "<Esc>", { silent = true })
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
