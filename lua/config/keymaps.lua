7fbf94e9e2f48c28d3298f04b8f1a205dfa5af38
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-10-27 08:41:59 -0300

    -- Disable arrow keys in normal mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,9 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Diagnostic keymaps
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+-- Disable arrow keys in normal mode :-)
+vim.keymap.set("n", "<left>", "<cmd>echo '😡 Use h to move!!'<CR>")
+vim.keymap.set("n", "<right>", "<cmd>echo '😡 Use l to move!!'<CR>")
+vim.keymap.set("n", "<up>", "<cmd>echo '😡 Use k to move!!'<CR>")
+vim.keymap.set("n", "<down>", "<cmd>echo '😡 Use j to move!!'<CR>")
