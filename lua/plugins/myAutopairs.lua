643bf0c40aa913971ab3b64aacff05f7d96b6835
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-10-27 14:48:09 -0300

    plugin settings in opts

--- lua/plugins/myAutopairs.lua
+++ lua/plugins/myAutopairs.lua
@@ -2,9 +2,6 @@ return {
   {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
-
-    config = function ()
-      require("nvim-autopairs").setup()
-    end
+    opts = {}
   }
 }
--- lua/plugins/myIndent.lua
+++ lua/plugins/myIndent.lua
@@ -2,16 +2,13 @@ return {
   {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
-
-    config = function ()
-      require("ibl").setup {
-        indent = {
-          char = "│",
-        },
-        scope = {
-          enabled = true,
-        },
-      }
-    end
+    opts = {
+      indent = {
+        char = "│",
+      },
+      scope = {
+        enabled = true,
+      },
+    }
   }
 }
