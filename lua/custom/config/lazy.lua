25b7ede350cd959057f5ef29c7e7d661525fe465
Gabriel Lima <gabrielgsdlima@gmail.com>
2025-02-02 19:17:01 -0300

    update lazy config

--- lua/custom/config/lazy.lua
+++ lua/custom/config/lazy.lua
@@ -2,14 +2,7 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "--branch=stable",
-    lazyrepo,
-    lazypath
-  })
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
@@ -25,15 +18,9 @@ vim.opt.rtp:prepend(lazypath)
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-    -- import your plugins
     { import = "custom.plugins" },
   },
-  ui = {
-    border = "rounded"
-  },
-  -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "default" } },
-  -- automatically check for plugin updates.
-  checker = { enabled = false },
-  change_detection = { notify = false }
+  checker = { enabled = true },
+  ui = { border = "rounded" }
 })
