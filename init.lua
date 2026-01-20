054317da587b02a546a9a4dfd44397e765b71ba9
Gabriel Lima <gabrielgsdlima@gmail.com>
2026-01-20 19:55:58 -0300

    add lua lsp

--- init.lua
+++ init.lua
@@ -70,3 +70,24 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = { "c", "cpp", "python" },
   callback = function() vim.treesitter.start() end,
 })
+
+
+-- LSP configs
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+vim.lsp.config["lua_ls"] = {
+  cmd = { "lua-language-server" },
+  filetypes = { "lua" },
+  root_markers = { { ".luarc.json", ".luarc.jsonc" }, ".git" },
+  settings = {
+    Lua = {
+      runtime = {
+        version = "LuaJIT",
+      }
+    }
+  }
+}
+
+
+-- LSP enabled
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+vim.lsp.enable("lua_ls")
